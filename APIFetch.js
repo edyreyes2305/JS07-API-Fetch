3ff6ba56b0443450506e73c9d2600dcfcbbf7ddf
Edgar Reyes <edyreyes2305@gmail.com>
2023-05-09 21:43:12 -0500

    Fix just one time insert data

--- APIFetch.js
+++ APIFetch.js
@@ -31,7 +31,7 @@ const avatar5 = document.getElementById("avatar5");
 const avatar6 = document.getElementById("avatar6");
 const tabla = document.getElementById("tabla");
 
-
+let bandera = true;
 
 console.log();
 const getData = async (httpUrl) => {
@@ -52,44 +52,49 @@ const getData = async (httpUrl) => {
 };
 
 const getLocalStorage = () => {
-    let dato = JSON.parse(localStorage.getItem("mis-datosguardados")); 
-    for (const i of dato) {
-        console.log(i.avatar);
-    } 
-    id1.innerHTML += dato[0].id;
-    id2.innerHTML += dato[1].id;
-    id3.innerHTML += dato[2].id;
-    id4.innerHTML += dato[3].id;
-    id5.innerHTML += dato[4].id;
-    id6.innerHTML += dato[5].id;
+    if(bandera) {
+        let dato = JSON.parse(localStorage.getItem("mis-datosguardados")); 
+        for (const i of dato) {
+            console.log(i.avatar);
+        } 
+        id1.innerHTML += dato[0].id;
+        id2.innerHTML += dato[1].id;
+        id3.innerHTML += dato[2].id;
+        id4.innerHTML += dato[3].id;
+        id5.innerHTML += dato[4].id;
+        id6.innerHTML += dato[5].id;
+        
+        email1.innerHTML += dato[0].email;
+        email2.innerHTML += dato[1].email;
+        email3.innerHTML += dato[2].email;
+        email4.innerHTML += dato[3].email;
+        email5.innerHTML += dato[4].email;
+        email6.innerHTML += dato[5].email;
+        
+        firstname1.innerHTML += dato[0].first_name;
+        firstname2.innerHTML += dato[1].first_name;
+        firstname3.innerHTML += dato[2].first_name;
+        firstname4.innerHTML += dato[3].first_name;
+        firstname5.innerHTML += dato[4].first_name;
+        firstname6.innerHTML += dato[5].first_name;
     
-    email1.innerHTML += dato[0].email;
-    email2.innerHTML += dato[1].email;
-    email3.innerHTML += dato[2].email;
-    email4.innerHTML += dato[3].email;
-    email5.innerHTML += dato[4].email;
-    email6.innerHTML += dato[5].email;
-    
-    firstname1.innerHTML += dato[0].first_name;
-    firstname2.innerHTML += dato[1].first_name;
-    firstname3.innerHTML += dato[2].first_name;
-    firstname4.innerHTML += dato[3].first_name;
-    firstname5.innerHTML += dato[4].first_name;
-    firstname6.innerHTML += dato[5].first_name;
+        lastname1.innerHTML += dato[0].last_name;
+        lastname2.innerHTML += dato[1].last_name;
+        lastname3.innerHTML += dato[2].last_name;
+        lastname4.innerHTML += dato[3].last_name;
+        lastname5.innerHTML += dato[4].last_name;
+        lastname6.innerHTML += dato[5].last_name;
+        
+        avatar1.src = dato[0].avatar;
+        avatar2.src = dato[1].avatar;
+        avatar3.src = dato[2].avatar;
+        avatar4.src = dato[3].avatar;
+        avatar5.src = dato[4].avatar;
+        avatar6.src = dato[5].avatar;
 
-    lastname1.innerHTML += dato[0].last_name;
-    lastname2.innerHTML += dato[1].last_name;
-    lastname3.innerHTML += dato[2].last_name;
-    lastname4.innerHTML += dato[3].last_name;
-    lastname5.innerHTML += dato[4].last_name;
-    lastname6.innerHTML += dato[5].last_name;
+        bandera = false;
+    }
     
-    avatar1.src = dato[0].avatar;
-    avatar2.src = dato[1].avatar;
-    avatar3.src = dato[2].avatar;
-    avatar4.src = dato[3].avatar;
-    avatar5.src = dato[4].avatar;
-    avatar6.src = dato[5].avatar;
     /* for (let i = 0; i < 6; i++) {
         const element = array[i];
         
@@ -98,5 +103,5 @@ const getLocalStorage = () => {
 
 }
 //getData(url);
-getLocalStorage();
+
 
